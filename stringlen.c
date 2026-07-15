ecdd8a4931da1261c42cab0cbea78e834cd1cf56
niyamat <niyamatkajal0104@gmail.com>
2026-07-15 21:47:11 +0530

    added stringlen.c

---
+++ stringlen.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+int my_strlen(const char *str) {
+	int length = 0;
+	while(str[length] != '\0') {
+		length ++;
+	}
+	return length;
+}
+	
+int main() {
+	char str[100];
+	scanf("%99s",str);
+	printf("Length = %d\n",my_strlen(str));
+	return 0;
+}
+	
